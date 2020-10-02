60df347212cc943d587d4a4a1e756f1fa51f0d0f
Lucas Antonio <lucasantonio0108@gmail.com>
2020-10-01 22:43:02 -0300

    atividade 2

--- Desktop/teste/projeto java/exercicios/src/exercicios/exercicio0707.java
+++ Desktop/teste/projeto java/exercicios/src/exercicios/exercicio0707.java
@@ -9,14 +9,20 @@ import javax.swing.*;
 public class exercicio0707 {
 public static void main(String[] args) {
 double peso,altura,imc;
-String nep="";
+String nep="0";
 nep = JOptionPane.showInputDialog(null,"Insira Peso");
 peso = Double.parseDouble(nep);
 nep = JOptionPane.showInputDialog(null,"Insira altura");
 altura = Double.parseDouble(nep);
 if(peso > 5/10  && altura > 1/10){
 imc = peso / (Math.pow(altura, 2));
+ JOptionPane.showMessageDialog(null, (imc < 18)? "desnutrido" :
+         (imc < 25) ? "peso normal" : (imc < 30)? "acima do peso" : (imc < 35)?
+                 "obesidade de primeiro grau" : (imc < 40)? " obesidade"
+                         + "de segundo grau":"obesidade de terceiro grau");
 
+}else{
+     JOptionPane.showMessageDialog(null,"os numeros estao errados");
 }
 
     
